f398038a37fc731dbb09417c5583740ac79b41b8
Adrian Chia <achia@nfsmith.com>
2015-04-13 13:51:50 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+GCM Demo
+========
+Android GCM demo with GCM Cloud Endpoint backend, based on https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/GcmEndpoints
+
+### built on
+Android Studio 1.2 Beta 3
+API Level 22 (Android 5.1)
+Min SDK: 16
---
+++ app/src/main/java/com/example/gcmdemo/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.gcmdemo;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.gcmdemo.backend.registration.Registration;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new GcmRegistrationAsyncTask(this).execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
+
+        private Registration regService = null;
+        private GoogleCloudMessaging gcm;
+        private Context context;
+
+        private static final String SENDER_ID = "REPLACE_ME_WITH_NUMERIC_PROJECT_ID";
+
+        public GcmRegistrationAsyncTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            if(regService == null) {
+                Registration.Builder builder = new Registration.Builder(
+                        AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                regService = builder.build();
+            }
+
+            String msg = "";
+            try {
+                if(gcm == null) {
+                    gcm = GoogleCloudMessaging.getInstance(context);
+                }
+                String regId = gcm.register(SENDER_ID);
+                msg = "Device registered, registration ID=" + regId;
+                regService.register(regId).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+                msg = "Error: " + e.getMessage();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg) {
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+            Logger.getLogger("REGISTRATION").log(Level.INFO, msg);
+        }
+    }
+}
